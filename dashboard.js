a5a4c072732b1532965fac8bbdf719872eaee90b
HemangJiwnani <120553443+HemangJiwnani@users.noreply.github.com>
2023-08-21 12:39:29 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -616,6 +616,7 @@ var jsonData;
   myHeaders.append("Content-Type", "application/json");
 
   document.getElementById("approve-btn")?.addEventListener("click", async (e) => {
+    console.log("I reached Here")
     e.preventDefault();
     // Get the current date and time
     // updateDatabase(obj.referenceId) Commented For Reference
